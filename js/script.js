1e77c4c8ead47a9cc7095871f1112bef89d838a6
lucasens1 <lucasensini1@gmail.com>
2024-04-11 15:19:33 +0200

    Project : init, milestone 0

---
+++ js/script.js
@@ -0,0 +1,60 @@
+/* Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	            scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg */
+
+
+/* 
+MILESTONE :zero::
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE :uno:
+Stampare su console le informazioni di nome, ruolo e la stringa della foto per ogni membro del team
+MILESTONE :due::
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1 :stella2::
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2 :stella2: :stella2:
+Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato. */
+
+
+//Milestone 0
+const teamArray = [
+    {
+        name : "Wayne",
+        surname : "Barnett",
+        role : "Founder & CEO",
+        image : "<img src:'./img/wayne-barnett-founder-ceo.jpg'>"
+    },
+    {
+        name : "Angela",
+        surname : "Caroll",
+        role : "Chief Editor",
+        image : "<img src:'./img/angela-caroll-chief-editor.jpg'>"
+    },
+    {
+        name : "Walter",
+        surname : "Gordon",
+        role : "Office Manager",
+        image : "<img src:'./img/walter-gordon-office-manager.jpg'>"
+    },
+    {
+        name : "Angela",
+        surname : "Lopez",
+        role : "Social Media Manager",
+        image : "<img src:'./img/angela-lopez-social-media-manager.jpg'>"
+    },
+    {
+        name : "Scott",
+        surname : "Estrada",
+        role : "Developer",
+        image : "<img src:'./img/scott-estrada-developer.jpg'>"
+    },
+    {
+        name : "Barbara",
+        surname : "Ramos",
+        role : "Graphic Designer",
+        image : "<img src:'./img/barbara-ramos-graphic-designer.jpg'>"
+    }
+]
